1aea943856fabdfa2b33a0d2393e93ac68012f60
mimuro-lab <mimurosyunya@gmail.com>
2020-04-05 19:14:19 +0900

    コマンドライン引数が入力されなかった時のエラー処理

--- EditImage/main.cpp
+++ EditImage/main.cpp
@@ -9,17 +9,16 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	//画像パスが入力されなかったら強制終了
+	if (argc == 1)	return 0;
 	string imagePath = argv[1];//入力画像ファイル名
 	string outputCsv = argv[1];//出力CSVファイル名
 	outputCsv = outputCsv.replace(outputCsv.end() - 4, outputCsv.end(), ".csv");
 	string outputImagePath = argv[1];//出力画像ファイル名
 	outputImagePath = outputImagePath.insert(outputImagePath.length() - 4, "_result");
 
-	
 	//Image2Csvクラスを用いて画像から画素地をRGB形式のCSVファイルへ変換する。
 	shared_ptr<Image2Csv> image = make_shared<Image2Csv>(imagePath, outputCsv);
-
-	
 	
 	//EditCsvクラスを用いてRGB形式の画素地からGrayscale形式へ変換する。
 	shared_ptr<EditCsv> editedCsv = make_shared<EditCsv>(outputCsv);
